ef1d668e17462db0a2397048eaf2b9c423c2250d
Pallavi Kathait <kathaitpallavi164@gmail.com>
2025-05-24 23:05:44 +0530

    modified some files

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource,Api,reqparse, fields, marshal_with, abort
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+api=Api(app)
 
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +15,76 @@ class UserModel(db.Model):
 
     def __repr__(self):
         return f"User(name = {self.name}, email = {self.name})"
+
+user_args = reqparse.RequestParser()
+user_args.add_argument('name', type=str, required=True, help="Name cannot be blank"
+                       )
+user_args.add_argument('email', type=str, required=True, help="Name cannot be blank")
+
+userFields={
+    'id':fields.Integer,
+    'name':fields.String,
+    'email':fields.String,
+}
+class Users(Resource):
+    @marshal_with(userFields)
+    def get(Self):
+        users = UserModel.query.all()
+        return users
+
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args["name"], email=args["email"])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 201
+    
+    class User(Resource):
+        @marshal_with(userFields)
+        def get(self, id):
+            user = UserModel.query.filter_by(id=id).first()
+            if not user:
+                abort(404, "üser not found")
+                return user
+            
+    class User(Resource):
+        @marshal_with(userFields)
+        def patch(self, id):
+            args = user_args.parse_args()
+            user = UserModel.query.filter_by(id=id).first()
+            if not user:
+                abort(404, "üser not found")
+                user.name = args["name"]
+                user.email= args["email"]
+                db.session.commit()
+                return user
+       
+        @marshal_with(userFields)
+        def patch(self, id):
+            args = user_args.parse_args()
+            user = UserModel.query.filter_by(id=id).first()
+            if not user:
+                abort(404, "üser not found")
+                user.name = args["name"]
+                user.email= args["email"]
+                db.session.commit()
+                return user
+
+        @marshal_with(userFields)
+        def delete(self, id):
+            args = user_args.parse_args()
+            user = UserModel.query.filter_by(id=id).first()
+            if not user:
+                abort(404, "üser not found")
+                db.session.delete(user)
+                db.session.commit()
+                users = UserModel.query.all()
+                return user, 204  
+
+api.add_resource(Users, '/api/users/')
+api.add_resource(Users, '/api/users/<int:id>')
 
 @app.route('/')
 def home():
